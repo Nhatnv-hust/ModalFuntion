d33ebe4f30a9292329ed6eb66195f7016e3b40a7
Nhatnv-hust <nhathoangcat97@gmail.com>
2022-08-27 21:27:23 +0700

    modal full

--- README.md
+++ README.md
@@ -1,24 +1,84 @@
-# project-morhn
+<template>
+  <div class="w-[700px] bg-[#ffffff] rounded items-center m-auto">
+    <div
+      class="
+        w-full
+        h-full
+        flex
+        justify-between
+        items-center
+        p-[15px]
+        text-[18px]
+        font-bold
+        border-solid border-[#e5e5e5] border-b-2
+      "
+    >
+      <div class="">
+        {{ label }}
+      </div>
+      <button @click="handleDelModal" class="m-[8px]">
+        <span>{{ icon }}</span>
+      </button>
+    </div>
+    <div class="p-[15px]">
+      <span class="font-normal my-[10px]">{{ subLable }}</span>
+      <div class="w-full flex justify-between items-center mt-[15px] gap-2">
+        <button
+          class="
+            w-[50%]
+            p-[5px]
+            text-[#337ab7] text-[14px] text-center
+            border-2 border-solid border-[#337ab7]
+            rounded
+            font-medium
+            break-all
+          "
+          @click="handleShowWantToBlock"
+        >
+          {{ contentText1 }}
+        </button>
+        <button
+          class="
+            w-[50%]
+            p-[5px]
+            text-[#337ab7] text-[14px] text-center
+            border-2 border-solid border-[#337ab7]
+            rounded
+            font-medium
+            break-all
+          "
+          @click="handleSetUnavailable"
+        >
+          {{ contentText2 }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
-## Project setup
-```
-yarn install
-```
+<script>
+export default {
+  name: "Modal",
+  props: {
+    label: String,
+    icon: String,
+    subLable: String,
+    contentText1: String,
+    contentText2: String,
+  },
+  methods: {
+    handleDelModal() {
+      this.$emit("handleDelModal");
+    },
+    handleSetUnavailable() {
+      this.$emit("handleSetUnavailable");
+    },
+    handleShowWantToBlock() {
+      this.$emit("handleShowWantToBlock");
+    },
+  },
+};
+</script>
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,222 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+  <div class="w-full h-full m-auto items-center">
+    <button class="p-[50px] m-10 text-[#111] bg-[blue]" @click="onToggleModal">
+      clickMe
+    </button>
+  </div>
+  <!-- nhớ thêm : lấy động -->
+  <teleport to="body">
+    <BaseModal v-if="isShowModal" @closeModal="onToggleModal">
+      <template v-slot:title>
+        <div
+          class="
+            flex
+            justify-between
+            items-start
+            p-4
+            rounded-t
+            border-b
+            dark:border-gray-100
+          "
+        >
+          <h3
+            class="text-xl font-semibold text-gray-900 dark:text-gray break-all"
+          >
+            <!-- {{ $t("reservation_calendar.modal.modal_label") }} -->
+            reservation_calendar.modal.modal_lab
+          </h3>
+          <button
+            @click="onToggleModal"
+            class="
+              text-gray-900
+              bg-transparent
+              hover:bg-gray-200 hover:text-gray-900
+              rounded-lg
+              text-sm
+              p-1.5
+              items-center
+              dark:hover:bg-gray-600 dark:hover:text-white
+            "
+          >
+            <span>X</span>
+          </button>
+        </div>
+      </template>
+      <template v-slot:content>
+        <div class="p-6 space-y-6">
+          <p class="text-base leading-relaxed break-all">
+            <!-- {{ $t("`reservation_calendar.modal.close_reservation") }} -->
+            reservation_calendar.modal.close_reservation
+          </p>
+        </div>
+      </template>
+      <template v-slot:foot>
+        <div class="flex items-center p-6 space-x-2 rounded-b break-all">
+          <button
+            @click="onModalConfirm"
+            class="
+              text-white
+              bg-blue-600
+              font-medium
+              rounded-lg
+              text-sm
+              inline-flex
+              items-center
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              break-all
+            "
+          >
+            <!-- {{ $t("`reservation_calendar.modal.close_reservation") }} -->
+            reservation_calendar.modal.close_reservation
+          </button>
+          <button
+            @click="onToggleModal"
+            class="
+              text-white
+              bg-blue-600
+              font-medium
+              rounded-lg
+              text-sm
+              inline-flex
+              items-center
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              break-all
+            "
+          >
+            <!-- {{ reservation_calendar.modal.set_unavailable }} -->
+            reservation_calendar.modal.set_unavailable
+          </button>
+        </div>
+      </template>
+    </BaseModal>
+  </teleport>
+  <teleport to="body">
+    <BaseModal v-if="isShowModalConfirm" @closeModal=onModalConfirm>
+      <template v-slot:content>
+        <div class="p-6 space-y-6">
+          <p class="text-base leading-relaxed break-all">
+            reservation_calendar.modal.close_reservation
+          </p>
+        </div>
+      </template>
+      <template v-slot:foot>
+        <div class="flex items-center p-6 space-x-2 rounded-b break-all">
+          <button
+            @click="onConfirmFinal"
+            class="
+              text-white
+              bg-blue-600
+              font-medium
+              rounded-lg
+              text-sm
+              inline-flex
+              items-center
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              break-all
+            "
+          >
+            reservation_calendar.modal.close_reservation
+          </button>
+          <button
+            @click="onModalConfirm"
+            class="
+              text-white
+              bg-blue-600
+              font-medium
+              rounded-lg
+              text-sm
+              inline-flex
+              items-center
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              break-all
+            "
+          >
+            reservation_calendar.modal.set_unavailable
+          </button>
+        </div>
+      </template>
+    </BaseModal>
+  </teleport>
+  <teleport to="body">
+    <BaseModal v-if="isConfirmFinal"  @closeModal=onConfirmFinal>
+      <template v-slot:content>
+        <div class="p-6 space-y-6">
+          <p class="text-base leading-relaxed break-all">
+            reservation_calendar.modal.close_reservation
+          </p>
+        </div>
+      </template>
+      <template v-slot:foot>
+        <div class="flex items-center p-6 space-x-2 rounded-b break-all">
+          <button
+            @click="onConfirmFinal"
+            class="
+              text-white
+              bg-blue-600
+              font-medium
+              rounded-lg
+              text-sm
+              inline-flex
+              items-center
+              px-5
+              py-2.5
+              text-center
+              mr-2
+              break-all
+            "
+          >
+            OK
+          </button>
+        </div>
+      </template>
+    </BaseModal>
+  </teleport>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { BaseModal: Modal },
+  name: "App",
+  setup() {
+    const isShowModal = ref(false);
+    const isShowModalConfirm = ref(false);
+    const isConfirmFinal = ref(false);
+    const onToggleModal = () => {
+      isShowModal.value = !isShowModal.value;
+    };
+    const onModalConfirm = () => {
+      isShowModal.value = false;
+      isShowModalConfirm.value = !isShowModalConfirm.value;
+    };
+    const onConfirmFinal = () => {
+      isShowModalConfirm.value = false;
+      isConfirmFinal.value = !isConfirmFinal.value;
+    };
+    return {
+      isShowModal,
+      isShowModalConfirm,
+      isConfirmFinal,
+      onToggleModal,
+      onModalConfirm,
+      onConfirmFinal,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
